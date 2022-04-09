667ea69391327f9b1d9266c498b4cdb5509fd37b
MykolaM-AVISPL <59696595+MykolaM-AVISPL@users.noreply.github.com>
2022-04-09 16:33:08 +0300

    Initial commit

---
+++ src/main/java/com/example/starter/StarterConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.starter;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Class StarterConfiguration that responsible for
+ *
+ * @author Mykola.Matsishin <br> created on 09 April 2022
+ * @since 5.5
+ */
+@Configuration
+public class StarterConfiguration {
+
+    
+}
